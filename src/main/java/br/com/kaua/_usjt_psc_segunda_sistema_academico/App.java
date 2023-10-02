ce39cb9007b0cef1f2814b7df9c8935c8b4dfefa
Kauã <kauaa.alternativo@gmail.com>
2023-10-02 20:58:25 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/kaua/_usjt_psc_segunda_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.kaua._usjt_psc_segunda_sistema_academico;
+
+/**
+ *
+ * @author 823151581
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
